36c0cc1d8dac44317c1325a8b14c3770bf431e56
AmienulRana <amienulrana@gmail.com>
2022-09-15 15:05:25 +0700

    mengubah mode menjadi production

--- config/index.ts
+++ config/index.ts
@@ -2,11 +2,11 @@ interface PropConfig {
     ROOT_API:string;
     ROOT_API_IMAGE:string;
 }
-const MODE = 'dev'; 
-
+const MODE = "prod"; 
+ 
 const CONFIG: PropConfig = {
-    ROOT_API: MODE === "dev" ? "http://localhost:5000/api/v1" : "https://apistoregg.herokuapp.com/api/v1",
-    ROOT_API_IMAGE:MODE === "dev" ? "http://localhost:5000/images" : "https://apistoregg.herokuapp.com/images",
+    ROOT_API: MODE !== "prod" ? "http://localhost:5000/api/v1" : "https://apistoregg.herokuapp.com/api/v1",
+    ROOT_API_IMAGE: MODE !== "prod" ? "http://localhost:5000/images" : "https://apistoregg.herokuapp.com/images",
 }
 
 export default CONFIG;
